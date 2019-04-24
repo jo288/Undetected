ea17b94912f32280d11b2718a9497c4c9ca8e53f
Jinju Ouck <jo288@cornell.edu>
2019-04-24 02:45:41 -0400

    box debug

---
+++ Undetected/core/assets/levels/InteriorWall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="InteriorWall" tilewidth="32" tileheight="48" tilecount="1" columns="1">
+ <image source="../textures/cropped_vwall.png" width="32" height="48"/>
+</tileset>
