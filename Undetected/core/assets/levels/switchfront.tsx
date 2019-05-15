1f934920a51c158c6f23f955acce03c780c239ef
Jinju Ouck <jo288@cornell.edu>
2019-05-15 03:45:10 -0400

    oops tiled bug

--- Undetected/core/assets/levels/switchfront.tsx
+++ Undetected/core/assets/levels/switchfront.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.3" name="switchfront" tilewidth="32" tileheight="32" tilecount="10" columns="7">
  <tileoffset x="0" y="-8"/>
- <image source="../textures/groundbutton2.png" width="224" height="32"/>
+ <image source="../textures/groundbutton_final.png" width="224" height="32"/>
 </tileset>
